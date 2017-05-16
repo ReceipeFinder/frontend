0f59246f31a766476d4eb14b9556641a61545aa5
Michal Grabowski <Miriloth@gmail.com>
2017-05-16 21:49:51 +0200

    Add user profile toggler

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import '../styles/main.less'
+import c from 'classnames'
+
 import UserProfile from '../components/UserProfile'
 import RecipeFinder from '../components/RecipeFinder'
 import RecipesList from '../components/RecipesList'
 
+import '../styles/main.less'
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      showUserProfile: false,
+      showRecipesList: false
+    };
+    
+    this.toggleUserProfile = this.toggleUserProfile.bind(this);
+  }
+  
+  toggleUserProfile() {
+    this.setState({showUserProfile: !this.state.showUserProfile})
+  }
+  
   render() {
+    
     return (
       <div className="main__layout">
-        <UserProfile/>
+        <div className={c({"user-profile__wrapper": true, "visible": this.state.showUserProfile})}>
+          <UserProfile/>
+          <div className="user-profile__toggler clickable" onClick={this.toggleUserProfile}>
+            <span className={c({
+              "pt-icon-standard": true,
+              "pt-icon-chevron-right": !this.state.showUserProfile,
+              "pt-icon-chevron-left": this.state.showUserProfile
+            })}/>
+          </div>
+        </div>
+        
         <RecipeFinder/>
         <RecipesList/>
       </div>
